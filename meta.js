ffdfeaf540952468078494fd7f51acb26483452b
ethancrist <ethancrist96@gmail.com>
2018-05-11 18:45:35 -0400

    Update all of master

---
+++ meta.js
@@ -0,0 +1,58 @@
+/**
+ * @title meta.js
+ * @description Server metadata.
+ * @author ethancrist
+ **/
+
+'use strict'
+
+// [DEPENDENCIES]
+const util = require('./util')
+
+
+// [DATA]
+const Meta = function(isStart) {
+    /**
+     * @description Metadata for this world.
+     **/
+    util.log('Loading world metadata...')
+
+    // Setting static prototype variables.
+    this.port = this.getPort()
+    this.isDevMode = process.env.WORLD === 'development'
+
+    this.options.hello = this.getStartupMessage()
+
+    // Metadata is being loaded from application start; storing startMS time.
+    if (isStart) this.startMS = util.getMS()
+}
+//Meta.prototype.getCache = function() { return this.isDevMode ? util.getMS() : meta.startMS },
+Meta.prototype.getPort = function() {
+    /**
+     * @description The port the server is running on.
+     **/
+    return process.env.PORT || 3000
+}
+Meta.prototype.getStartupMessage = function() {
+    /**
+     * @description The message that logs when the application starts listening.
+     **/
+    return 'World is now online on port ' + this.port + '.'
+}
+Meta.prototype.options = {
+    /**
+     * @description The start options passed through to iomicro on .listen()
+     **/
+    appName: 'Paperweight-W' + process.env.WORLD,
+    viewDir: '../client',
+    ssl: {
+        key: '/etc/letsencrypt/live/narrownode.com/privkey.pem',
+        cert: '/etc/letsencrypt/live/narrownode.com/cert.pem',
+        forceHTTPS: true
+    },
+} 
+
+// [EXPORTS]
+module.exports = function(isStart) {
+    return new Meta(isStart)
+}
---
+++ util.js
@@ -0,0 +1,52 @@
+/**
+ * @title util.js
+ * @description Basic server utilities.
+ * @author ethancrist
+ **/
+
+'use strict'
+
+// [DEPENDENCIES]
+const micro = require('iomicro')
+const clockwork = require('clockwork')({ key: '8b3e9f290c3b39837f22b5654c3550cf641b6f72' })
+
+
+// [DATA]
+const Util = function() {
+    /**
+     * @description Toolkit for engine developers.
+     **/
+    this.micro = micro
+    this.log = micro.log
+    this.log('Loading utilities...')
+}
+Util.prototype.getMS = function() {
+    /**
+     * @returns The current time since last epoch in MS.
+     **/
+    return new Date().getTime() 
+}
+Util.prototype.sendText = function(message, number) {
+    /**
+     * @purpose Send a text message.
+     * @note Format: (555) 123-4567 = '15551234567'
+     **/
+
+    // Fallback phone number.
+    number = number || '15133302458'
+
+    // Wrapping text message operation in a Promise.
+    return new Promise(function(resolve, reject) {
+        clockwork.sendSms({ To: number.toString(), Content: message }, (err, res) => {
+            // Running the Promise resolve or reject.
+            err ? reject(err) : resolve(res)
+            
+            // Logging the text message response.
+            err ? micro.error(err) : micro.log(JSON.stringify(res))
+        })
+    })
+}
+
+
+// [EXPORTS]
+module.exports = new Util()
